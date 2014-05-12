34a2d0e1572e10836818b88dc13166107e455ded
Aurelien M <a_merzeau@live.fr>
2014-05-12 20:26:33 +0200

    test

--- phu/Models/AccountModels.cs
+++ phu/Models/AccountModels.cs
@@ -11,8 +11,8 @@ namespace phu.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            //: base("phu_bddEntities")
-            : base("DefaultConnection")
+            : base("phu_bddEntities")
+            //: base("DefaultConnection")
         {
         }
 
